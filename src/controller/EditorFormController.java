4019d988727be3fec349f2b87a73237fcb40ff97
Charith Gimhana <charithgimhana97@gmail.com>
2021-06-19 20:50:07 +0530

    FXUtil class created

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -1,12 +1,15 @@
 package controller;
 
+import com.sun.java.util.jar.pack.ConstantPool;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class EditorFormController {
     public TextArea txtEditor;
@@ -17,6 +20,24 @@ public class EditorFormController {
 
     public  void  initialize(){
         pneFind.setVisible(false);
+
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            FXUtil.highlightOnTextArea(txtEditor,newValue, Color.web("yellow", 0.8));
+
+            try {
+                Pattern regExp = Pattern.compile(newValue);
+                Matcher matcher = regExp.matcher(txtEditor.getText());
+
+                searchList.clear();
+
+                while (matcher.find()) {
+                    searchList.add(new ConstantPool.Index(matcher.start(), matcher.end()));
+                }
+            } catch (PatternSyntaxException e) {
+
+            }
+        });
     }
 
 
@@ -54,9 +75,25 @@ public class EditorFormController {
         if (exists){
             findOffSet=matcher.start() + 1;
             txtEditor.selectRange(matcher.start(), matcher.end());
+        }else {
+            findOffSet=0;
         }
     }
 
     public void btnFindPrevious_OnAction(ActionEvent actionEvent) {
+        String pattern=txtSearch.getText();
+        String text=txtEditor.getText();
+
+        Pattern regEx=Pattern.compile(pattern);
+        Matcher matcher=regEx.matcher(text);
+
+        boolean exists = matcher.find(findOffSet);
+        if (exists){
+            findOffSet=matcher.start() - 1;
+            txtEditor.selectRange(matcher.start(), matcher.end());
+        }else {
+            findOffSet=0;
+        }
+
     }
 }
