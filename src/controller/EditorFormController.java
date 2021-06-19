aa6e0486a310ec70e68ae96c403547c6f21e3eb4
Charith Gimhana <charithgimhana97@gmail.com>
2021-06-19 17:31:33 +0530

    The method fnd next was created

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -5,10 +5,22 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EditorFormController {
     public TextArea txtEditor;
     public AnchorPane pneFind;
     public TextField txtSearch;
+
+    private int findOffSet;
+
+    public  void  initialize(){
+        pneFind.setVisible(false);
+    }
+
+
+
 
     public void mnuItemNew_OnAction(ActionEvent actionEvent) {
         txtEditor.clear();
@@ -19,6 +31,8 @@ public class EditorFormController {
     }
 
     public void mnuItemFind_OnAction(ActionEvent actionEvent) {
+        pneFind.setVisible(true);
+        txtSearch.requestFocus();
     }
 
     public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
@@ -30,6 +44,17 @@ public class EditorFormController {
 
 
     public void btnFindNext_OnAction(ActionEvent actionEvent) {
+        String pattern=txtSearch.getText();
+        String text=txtEditor.getText();
+
+        Pattern regEx=Pattern.compile(pattern);
+        Matcher matcher=regEx.matcher(text);
+
+        boolean exists = matcher.find(findOffSet);
+        if (exists){
+            findOffSet=matcher.start() + 1;
+            txtEditor.selectRange(matcher.start(), matcher.end());
+        }
     }
 
     public void btnFindPrevious_OnAction(ActionEvent actionEvent) {
